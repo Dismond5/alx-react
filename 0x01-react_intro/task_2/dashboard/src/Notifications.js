79168037bfdc45884d6dc666e7b78266cee8f41f
Omar <omarkhaled201148@gmail.com>
2023-11-14 19:34:30 +0200

    a

--- 0x01-react_intro/task_2/dashboard/src/Notifications.js
+++ 0x01-react_intro/task_2/dashboard/src/Notifications.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import './notifications.css';
+import { getLatestNotification } from './utils';
+
 
 function Notifications() {
     return(
         <div classname = 'Notifications'>
             <p>Here is the list of notifications</p>
+            <button aria-label="Close" onClick={console.log('Close button has been clicked')}>
+            <img src="/close-icon.png" alt="close-icon" />
+            </button>
+            <ul>
+                <li data="default">New course available</li>
+                <li data="urgent">New resume available</li>
+                <li dangerouslySetInnerHTML={{ __html: getLatestNotification() }}data="urgent"></li>
+            </ul>
         </div>
     )
 }
