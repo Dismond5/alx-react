608cc9979531683fdf917431fd3e8f1827effeaa
Omar <omarkhaled201148@gmail.com>
2023-11-14 18:45:19 +0200

    a

---
+++ 0x01-react_intro/task_1/dashboard/src/App.js
@@ -0,0 +1,22 @@
+import logo from './helberton-logo.jpg';
+import './App.css';
+import { getFullYear, getFooterCopy } from './utils';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>School dashboard</h1>
+      </div>
+      <div classname = "App-body">
+        <p>Login to access the full dashboard</p>
+      </div>
+      <div classname = "App-footer">
+        <p>Copyright {getFullYear()} - {getFooterCopy(true)}</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
